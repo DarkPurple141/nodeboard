1055ac4cc88fd2405281f1820f35868d57b8ac70
Alex Hinds <alex.hinds141@gmail.com>
2017-09-07 14:38:25 +1000

    latest updates

--- public/scripts/got-front.js
+++ public/scripts/got-front.js
@@ -3,76 +3,16 @@ This file is currently a total disaster.
 
 Please avert your eyes.
 
-Blame @DarkPurple141
+@blame @DarkPurple141
 
 **/
 
+// import {regions, Game} from "logic/engine.js"
 
-var regions = {
-  "cb" : {
-    name : "Castle Black",
-    id : 1,
-    neighbours: [2, 3, 4, 5],
-    icon: "crown",
-    type: "land"
-  },
-  "kh" : {
-    name : "Karhold",
-    id : 2,
-    neighbours: [1, 3, 4],
-    icon: "crown",
-    type: "land"
-  },
-  "ss" : {
-    name : "The Shivering Sea",
-    id : 3,
-    neighbours: [1, 2, 4, 6, 7, 8],
-    icon: null,
-    type: "sea"
-  },
-  "wf" : {
-    name : "Winterfell",
-    id : 4,
-    neighbours: [1, 2, 3, 5, 6, 9, 10],
-    icon: ["crown", "barrel"],
-    type: "garrison",
-    harbor: 5
-  },
-  "bi" : {
-    name : "Bay of Ice",
-    id : 5,
-    neighbours: [1, 4, 9 ],
-    icon: null,
-    type: "sea"
-  }
-}
+const socket = io.connect('http://localhost:3000');
+const game = new Game(socket);
 
-
-var map = $('div#map');
-
-for (var reg in regions) {
-  var curr = regions[reg];
-  console.log(curr);
-  var $div = $("<div>", { id: curr['id'], "class": "region"});
-  $div.text(curr['name']);
-  map.append($div);
-}
-
-
-function Game(socket) {
-  this.userName = "Anon";
-  this.socket = socket;
-  this.players = [];
-}
-
-Game.prototype.x = function () {
-  console.log("load");
-  this.socket.emit('game-event', {gameState: "Nice" });
-};
-
-var socket = io.connect('http://localhost:3000');
-var game = new Game(socket);
-
+// network stuff
 socket.on('news', function (data) {
   console.log(data);
   //socket.emit('my other event', { my: 'data' });
@@ -81,5 +21,19 @@ socket.on('news', function (data) {
 socket.on('update', function (data) {
   console.log("updating!");
 });
+
+/// eventuall this will create the svg map
+$(document).ready(
+  () => {
+    let map = $('div#map');
+    for (let reg in regions) {
+      let curr = regions[reg];
+      console.log(curr);
+      let div = $("<div>", { id: curr['id'], "class": "region"} );
+      div.text(curr['name']);
+      //map.append(div);
+      map.append(div);
+    }
+})
 
 window.game = game;
---
+++ public/scripts/logic/engine.js
@@ -0,0 +1,89 @@
+// static map information and Game Engine
+
+const regions = [
+  {
+    id: 0
+  },
+  {
+    name : "Castle Black",
+    id : 1,
+    neighbours: [2, 3, 4, 5],
+    icon: "crown",
+    type: "land"
+  },
+  {
+    name : "Karhold",
+    id : 2,
+    neighbours: [1, 3, 4],
+    icon: "crown",
+    type: "land"
+  },
+  {
+    name : "The Shivering Sea",
+    id : 3,
+    neighbours: [1, 2, 4, 6, 7, 8],
+    icon: null,
+    type: "sea"
+  },
+  {
+    name : "Winterfell",
+    id : 4,
+    neighbours: [1, 2, 3, 5, 6, 9, 10],
+    icon: ["crown", "barrel"],
+    type: "garrison",
+    harbor: 5
+  },
+  {
+    name : "Bay of Ice",
+    id : 5,
+    neighbours: [1, 4, 9, 11, 12, 13],
+    icon: null,
+    type: "sea"
+  },
+  {
+    name : "White Harbor",
+    id : 6,
+    neighbours: [3, 4, 7, 8, 10],
+    icon: null,
+    type: "castle",
+  },
+  {
+    name : "Widow's Watch",
+    id : 7,
+    neighbours: [3, 6, 8],
+    icon: ["barrel"],
+    type: "land"
+  },
+  {
+    name : "The Narrow Sea",
+    id : 8,
+    neighbours: [3, 6, 7, 10, 14, 15, 16, 17, 27, 28],
+    icon: null,
+    type: "sea"
+  },
+  {
+    name : "The Stoney Shore",
+    id : 9,
+    neighbours: [4, 5],
+    icon: ["barrel"],
+    type: "land"
+  },
+  {
+    name : "Moat Calin",
+    id : 10,
+    neighbours: [4, 6, 8, 11, 14, 19],
+    icon: ["barrel"],
+    type: "land"
+  }
+]
+
+function Game(socket) {
+  this.userName = "Anon";
+  this.socket = socket;
+  this.players = [];
+}
+
+Game.prototype.x = function () {
+  console.log("load");
+  this.socket.emit('game-event', {gameState: "Nice" });
+};
