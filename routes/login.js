863802a0ca303e7d2e6fbdfa4cd2a545c4183bdc
Zain Afzal <zain.afz@gmail.com>
2017-09-21 23:28:11 +1000

    removed wrong comment

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 /* GET home listing. */
 router.get('/', function(req, res, next) {
-  // Note that root component is special
-  // in that it must be placed last
   res.render('base', {
     components: ["navbar","footer","login"],
     layout: false
