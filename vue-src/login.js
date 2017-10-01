8a4a72c9666decc40f25643db534ae8530e4a2af
Zain Afzal <zain.afz@gmail.com>
2017-10-01 21:49:15 +1100

    some basic vuetify stuff for login/landing page

--- vue-src/login.js
+++ vue-src/login.js
@@ -1,10 +1,24 @@
 // main app decleration
 Vue.component('app', {
 	template: `
-		<div>
-			<v-container>Hello world</v-container>
-  	</div>`,
-  data: function(){
-  	return {};
-  }
+		<v-app light>
+			<main>
+				<section>
+						<v-parallax src="/images/landing.png"  height="600">
+							<v-layout column align-center justify-center>
+								<h1 class="white--text orbit-font">Nodeboard</h1>
+							<h4 class="white--text orbit-font">Boardgames Reimagined</h4>
+						</v-layout>
+					</v-parallax>
+				</section>
+				<section>
+				
+				</section>
+			</main>
+		</v-app>`,
+	data: function(){
+		return {
+			title: "Nodeboard"
+		};
+	}
 });
