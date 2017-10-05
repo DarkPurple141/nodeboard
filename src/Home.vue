ec8f1ef663b617e51f2a18b79cccb65747942905
Anna Bardsley <anna.bardsley@gmail.com>
2017-10-05 18:15:43 +1100

    slowly understanding vue

--- src/Home.vue
+++ src/Home.vue
@@ -19,42 +19,7 @@
          <v-flex xs12>
            <v-container grid-list-xl>
              <v-layout row wrap align-center>
-               <v-flex xs12 md4>
-                 <v-card class="elevation-0 transparent">
-                   <v-card-title primary-title class="layout justify-center">
-                     <div class="headline text-xs-center amber--text">Play!</div>
-                   </v-card-title>
-                   <v-card-text>
-                     Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                     Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                   </v-card-text>
-                 </v-card>
-               </v-flex>
-               <v-flex xs12 md4>
-                 <v-card class="elevation-0 transparent">
-                   <v-card-title primary-title class="layout justify-center">
-                     <div class="headline text-xs-center amber--text">Create!</div>
-                   </v-card-title>
-                   <v-card-text>
-                     Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                     Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                   </v-card-text>
-                 </v-card>
-               </v-flex>
-               <v-flex xs12 md4>
-                 <v-card class="elevation-0 transparent">
-                   <v-card-title primary-title class="layout justify-center">
-                     <div class="headline text-xs-center amber--text">Compete!</div>
-                   </v-card-title>
-                   <v-card-text>
-                     Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                     Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                   </v-card-text>
-                 </v-card>
-               </v-flex>
+               <description-card v-for="item in cards" :cardData="item"></description-card>
              </v-layout>
            </v-container>
          </v-flex>
@@ -66,13 +31,37 @@
  </template>
 
  <script>
+ import descriptionCard from './components/description-card'
 
  export default {
-   name: 'App',
+   name: 'Home',
    data: function(){
      return {
-       title: "NodeBoard"
+       title: "NodeBoard",
+       cards : [
+          {
+            headline: "Play!",
+            stub: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.\
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.\
+            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+          },
+          {
+            headline: "Create!",
+            stub: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.\
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.\
+            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+          },
+          {
+            headline: "Compete!",
+            stub: "Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.\
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.\
+            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti."
+          }
+       ]
      };
+   },
+   components: {
+    descriptionCard
    }
  }
 </script>
---
+++ src/components/description-card.vue
@@ -0,0 +1,24 @@
+<template>
+   <v-flex xs12 md4>
+     <v-card class="elevation-0 transparent">
+      <v-card-title primary-title class="layout justify-center">
+         <div class="headline text-xs-center amber--text">{{ cardData.headline }}</div>
+      </v-card-title>
+      <v-card-text>
+         {{ cardData.stub }}
+      </v-card-text>
+     </v-card>
+   </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'descriptionCard',
+  template: "<description-card></description-card>",
+  // note cardInfo tranlsates to card-info in html render.
+  props: ['cardData'],
+  mounted() {
+      console.log('Component ready.')
+  }
+}
+</script>
