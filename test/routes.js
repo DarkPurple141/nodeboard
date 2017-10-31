f4ebc73a2910628ad32e70a3f08607e86b8fe316
Alex Hinds <alex.hinds141@gmail.com>
2017-10-31 15:19:44 +1100

    Added mongoose promise library

--- test/routes.js
+++ test/routes.js
@@ -1,20 +1,21 @@
 const app = require("../app")("test2");
 const supertest = require("supertest")(app);
 const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
 
 describe("Route testing", function() {
    it("Test index", function(done) {
        supertest
            .get("/")
            .expect(200)
-           .end(done);
+           .end(done)
    })
 
    it("Test api", function(done) {
        supertest
            .get("/api")
            .expect(200)
-           .end(done);
+           .end(done)
    })
 
 
@@ -22,21 +23,21 @@ describe("Route testing", function() {
        supertest
            .get("/api/user")
            .expect(209) // no user logged in
-           .end(done);
+           .end(done)
    })
 
    it("Test 404", function(done) {
        supertest
            .get("/shizzlwazzle")
            .expect(404)
-           .end(done);
+           .end(done)
    })
 
    it("Test api/play", function(done) {
        supertest
            .get("/api/play")
            .expect(200)
-           .end(done);
+           .end(done)
    })
 
    after(function(done) {
