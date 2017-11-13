94197d932d0ee12b851359193318b8f68041b19d
Alex Hinds <alex.hinds141@gmail.com>
2017-11-13 14:15:52 +1100

    [RISK] refactored a lot of the front to clean it up

---
+++ src/components/boards/risk/regions.vue
@@ -0,0 +1,20 @@
+<template>
+<g class="territory">
+   <path v-for="path in region.paths" v-bind:d="path"></path>
+</g>
+</template>
+
+<script type="text/javascript">
+   export default {
+      name: "Region",
+      props: ['region']
+   }
+</script>
+
+<style>
+.territory {
+  stroke-width: 1px;
+  stroke: #000000;
+}
+
+</style>
